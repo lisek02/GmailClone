41d7b0c7cdbd1c201dea1aeea4b5a42eac873161
Wojtek Lisiecki <wojciech.j.lisiecki@gmail.com>
2015-03-22 20:13:00 +0100

    add email factory

--- app/emailModule.js
+++ app/emailModule.js
@@ -1,6 +1,18 @@
 (function() {
 angular.module('emailModule', [])
 
+.controller('EmailController', ['$scope', '$stateParams', 'emailFactory', function($scope, $stateParams, emailFactory) {
+	this.email = [];
+
+	emailFactory.getEmail($stateParams)
+		.success( angular.bind(this, function(jsonData, statusCode) {
+			console.log('The request was successcul', statusCode, jsonData);
+			this.email = jsonData;
+		}) );
+
+	console.log('Email details:', this.email);
+}])
+
 .directive('email', function(){
 	// Runs during compile
 	return {
@@ -9,6 +21,22 @@ angular.module('emailModule', [])
 		link: function($scope, iElm, iAttrs, controller) {			
 		}
 	};
-});
+})
+
+.factory('emailFactory', function emailFactory($http, $stateParams) {
+	var exports = {};
+
+	exports.getEmail = function(params) {
+		if(params.id) {
+			return $http.get('app/json/message/' + params.id + '.json')
+				.error(function(data) {
+					console.log('There was an error!', data);
+				});
+		};
+	};
+
+	return exports;		
+
+})
 
 })();
--- app/inboxModule.js
+++ app/inboxModule.js
@@ -1,5 +1,5 @@
 (function() {
-angular.module('inboxModule', [])
+angular.module('inboxModule', ['ngSanitize'])
 
 .controller('InboxController', ['$scope', 'inboxFactory', function($scope, inboxFactory){
 	this.messages = [];
